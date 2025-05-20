5d5be99fff8790aab61c2db9ccb43b1907a57532
mgggai <info@oday.kz>
2025-05-20 14:42:01 +0500

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,22 @@ import IMask from "imask";
 import Lenis from "lenis";
 // import TabsComponent from "./components/tabs";
 // import AccordionComponent from './components/accordeon.js';
+// import Swiper from "swiper";
+// import { Navigation, Parallax, Thumbs, EffectFade } from "swiper/modules";
+// import "swiper/css/bundle";
+
+// const sliderReviews = new Swiper(".reviews-slider", {
+// 	modules: [Navigation],
+// 	speed: 1000,
+// 	loop: true,
+// 	clickable: true,
+// 	slidesPerView: 2,
+// 	spaceBetween: 20,
+// 	navigation: {
+// 		prevEl: "#btn-slider-prev ",
+// 		nextEl: "#btn-slider-next ",
+// 	},
+// });
 
 const lenis = new Lenis({
 	autoRaf: true,
@@ -11,11 +27,33 @@ const lenis = new Lenis({
 lenis.on("scroll", (e) => {});
 
 const lightboxForm = GLightbox({
-	selector: ".popup-form",
+	selector: ".popup-with-form",
 	openEffect: "zoom",
 	closeEffect: "fade",
 	touchNavigation: false,
+	loop: false,
+	slideShow: false,
+	closeButton: false,
+	draggable: false,
+	keyboardNavigation: false,
+	beforeSlideLoad: (slide) => {
+		const arrows = document.querySelectorAll(".gprev, .gnext");
+		arrows.forEach((el) => (el.style.display = "none"));
+	},
 });
+
+lightboxForm.on("open", () => {
+	const inputs = document.querySelectorAll(".phone");
+	const maskOptions = {
+		mask: "+{1}(000)000-00-00",
+	};
+	inputs.forEach((input) => {
+		IMask(input, maskOptions);
+	});
+});
+
+
+
 
 document.querySelectorAll(".form").forEach(function (form) {
 	form.addEventListener("submit", function (e) {
